f68daa3e501e18babed42b91346b488dfacd7242
siattesting <siat.testing@gmail.com>
2024-11-01 17:02:53 +0000

    minor updates

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kibily/flaskloyaltypoints/.venv"
+set -gx VIRTUAL_ENV "/home/kibily/flaskpointssql/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
